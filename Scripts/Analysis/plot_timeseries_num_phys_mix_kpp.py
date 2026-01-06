93c5e8d86740537a6ef8600608dad9e97208bb9c
Brianna Undzis <bundzis@login37.chn.perlmutter.nersc.gov>
2026-01-06 15:26:35 -0800

    made version to post-process and save kpp output

---
+++ Scripts/Analysis/plot_timeseries_num_phys_mix_kpp.py
@@ -0,0 +1,361 @@
+#################### Numerical & Physical Mixing in Ideal ROMS-Budgell Beaufort Jet ########################
+# The purpose of this script is to plot time series of volume-averaged numerical and physical 
+# mixing for salinity and temperature. Other processing for time series might be added in here, too,
+# but all post-processed data will be save to netCDFs so the analysis does not need to be redone 
+# to replot things. 
+#
+# Notes:
+# - This script runs in xroms 
+# - This script heavily uses code from Dylan Schlichting 
+# - Other things that will maybe be added into here: energy analysis of eddy things, 
+#   M2 and N2, ice volume/concentration/whatever is standard in papers
+# - Choose an area that always has eddies, vertical slices constrained to top 200 m, 
+# - I think I am going to add things for ice variables into here...
+# - This is the same as the .ipynb version of this script but ran for just one model output
+#   so that I can run them all at the same time since they take so long -- the output for this one 
+#   is using the kpp vertical mixing scheme 
+
+##############################################################################################################
+
+
+# Load in the packages
+import numpy as np
+import cartopy
+import glob
+import cartopy.crs as ccrs
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+import cartopy.feature as cfeature
+import cmocean.cm as cmo
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tick
+import warnings
+import xarray as xr
+import xroms
+from matplotlib import ticker
+crs = ccrs.PlateCarree()
+warnings.filterwarnings("ignore") #turns off annoying warnings
+#Cartopy
+land_10m = cfeature.NaturalEarthFeature('physical', 'land', '10m',
+                                edgecolor='face',
+                                facecolor=cfeature.COLORS['land'])
+
+# The core of the analysis is done with xhistogram
+from xhistogram.xarray import histogram
+
+
+# ---------------------------------------------------------------------------
+# -------------------------- Define Functions -------------------------------
+# ---------------------------------------------------------------------------
+
+# Make a function to open the model output
+def open_roms(path):
+    '''
+Opens multiple netcdf files with xroms
+    '''
+    # chunk = {"xi": -1, "eta": -1, "ocean_time": -1} 
+    # chunks = {}
+    # for sub in ["rho", "u", "v", "psi"]:
+        # for k, v in chunk.items():
+            # chunks[f"{k}_{sub}"] = v
+    # chunks["ocean_time"] = chunk["ocean_time"]
+
+    ds = xroms.open_netcdf(path)
+    ds,grid = xroms.roms_dataset(ds,include_cell_volume=True)
+    ds.xroms.set_grid(grid)
+    return ds,grid
+
+
+def open_mfroms(path):
+    '''
+Opens multiple netcdf files with xroms
+    '''
+    # chunk = {"xi": -1, "eta": -1, "ocean_time": -1} 
+    # chunks = {}
+    # for sub in ["rho", "u", "v", "psi"]:
+        # for k, v in chunk.items():
+            # chunks[f"{k}_{sub}"] = v
+    # chunks["ocean_time"] = chunk["ocean_time"]
+
+    ds = xroms.open_mfnetcdf(path)
+    ds,grid = xroms.roms_dataset(ds,include_cell_volume=True)
+    ds.xroms.set_grid(grid)
+    return ds,grid
+
+
+# Make a function to calculate useful derivatives and add them back into 
+# the dataset
+def add_derivatives(ds, grid, q='salt'):
+    '''
+Analysis of velocity gradient tensor and frontogenesis function
+following Hetland et al. (2025) JPO
+    '''
+    qs = ds[q]
+    
+    #############################
+    # Flow and property gradients at the ocean surface
+    
+    ds['dqdx'] = xroms.to_rho(grid.derivative(qs.isel(s_rho=-1), 'X'), grid)    # defined at rho-points
+    ds['dqdy'] = xroms.to_rho(grid.derivative(qs.isel(s_rho=-1), 'Y'), grid)    # defined at rho-points
+    
+    ds['dudx'] = grid.derivative(ds.isel(s_rho=-1).u, 'X', boundary='extend')  # defined at rho-points
+    ds['dvdy'] = grid.derivative(ds.isel(s_rho=-1).v, 'Y', boundary='extend')  # defined at rho-points
+    ds['dvdx'] = xroms.to_rho(grid.derivative(ds.isel(s_rho=-1).v, 'X', boundary='extend'), grid)  # defined at rho-points
+    ds['dudy'] = xroms.to_rho(grid.derivative(ds.isel(s_rho=-1).u, 'Y', boundary='extend'), grid)  # defined at rho-points
+    
+    ###########################
+    # Invariant flow properties
+    
+    # Vorticity:  v_x - u_y
+    ds['zeta'] = (ds.dvdx - ds.dudy)/ds.f
+    ds['zeta'].name = 'Normalized vorticity'
+
+    # Divergence: u_x + v_y
+    ds['delta'] = (ds.dudx + ds.dvdy)/ds.f
+    ds['delta'].name = 'Normalized divergence'
+
+    # Major axis of deformation
+    ds['alpha'] = ( np.sqrt( (ds.dudx-ds.dvdy)**2 + (ds.dvdx+ds.dudy)**2 ) )/ds.f
+    ds['alpha'].name = 'Normalized total strain'
+
+    ##################################
+    # Principle deformation components
+
+    ds['lminor'] = 0.5 * (ds.delta - ds.alpha)
+    ds['lminor'].name = 'lambda minor'
+
+    ds['lmajor'] = 0.5 * (ds.delta + ds.alpha)
+    ds['lmajor'].name = 'lambda major'
+    
+    #############################################
+    # Along- and cross-frontal velocity gradients
+    
+    # angle is wrt x, so need to do arctan2(y, x)
+    ds['phi_cf'] = np.arctan2(ds.dqdy, ds.dqdx)
+    ds['phi_af'] = ds.phi_cf + np.pi/2.0
+
+    ds['du_cf'] = ( ds.dudx*np.cos(ds.phi_cf)**2 + ds.dvdy*np.sin(ds.phi_cf)**2 
+               + (ds.dudy + ds.dvdx)*np.sin(ds.phi_cf)*np.cos(ds.phi_cf) )/ds.f
+
+    ds['du_af'] = ( ds.dudx*np.cos(ds.phi_af)**2 + ds.dvdy*np.sin(ds.phi_af)**2
+              + (ds.dudy + ds.dvdx)*np.sin(ds.phi_af)*np.cos(ds.phi_af) )/ds.f
+    
+    ############################
+    # The frontogenesis function
+    
+    # Dimensional frontogenesis function
+    Dgradq_i = - ds.dudx*ds.dqdx - ds.dvdx*ds.dqdy
+    Dgradq_j = - ds.dudy*ds.dqdx - ds.dvdy*ds.dqdy
+    ds['Ddelq2'] = (ds.dqdx*Dgradq_i + ds.dqdy*Dgradq_j)
+    ds['Ddelq2'].name = 'Frontogenesis function'
+
+    # Density gradients squared
+    ds['gradq2'] = ds.dqdx**2 + ds.dqdy**2
+    ds['gradq2'].name = r'$(\nabla q)^2$'
+
+    # Normalized frontogenesis function
+    ds['nFGF'] = 0.5 * ds.Ddelq2 / (ds.gradq2 * ds.f)
+    ds['nFGF'].name = r'Normalized Frontogenesis Function'
+    
+    return ds
+
+
+# ---------------------------------------------------------------------------
+# -------------------------- Process Output -------------------------------
+# ---------------------------------------------------------------------------
+
+
+# Constant forcing (time series), ice, U3C4, K-eps
+#path = glob.glob('/global/cfs/cdirs/m4572/dylan617/beaufort_jet/runs/roms_avg_ice_500m_w_dvd_constant_forcing_u3c4_keps*.nc')
+# Constant forcing, U3C4, gen
+# grd_file = '/global/homes/b/bundzis/Projects/Beaufort_ROMS_idealized_jet/Include/grd_1000_m.nc'
+#path_const_u3c4_gen = glob.glob('/global/cfs/cdirs/m4572/dylan617/beaufort_jet/runs/roms_avg_ice_500m_w_dvd_constant_forcing_u3c4_gen_*.nc')
+# Constant forcing, U3C4, kkl
+# grd_file = '/global/homes/b/bundzis/Projects/Beaufort_ROMS_idealized_jet/Include/grd_1000_m.nc'
+#path_const_u3c4_kkl = glob.glob('/global/cfs/cdirs/m4572/dylan617/beaufort_jet/runs/roms_avg_ice_500m_w_dvd_constant_forcing_u3c4_kkl_*.nc')
+# Constant forcing, U3C4, k-omega
+# grd_file = '/global/homes/b/bundzis/Projects/Beaufort_ROMS_idealized_jet/Include/grd_1000_m.nc'
+#path_const_u3c4_komega = glob.glob('/global/cfs/cdirs/m4572/dylan617/beaufort_jet/runs/roms_avg_ice_500m_w_dvd_constant_forcing_u3c4_komega_*.nc')
+# Constant forcing, U3C4, kpp
+# grd_file = '/global/homes/b/bundzis/Projects/Beaufort_ROMS_idealized_jet/Include/grd_1000_m.nc'
+path_const_u3c4_kpp = glob.glob('/global/cfs/cdirs/m4572/dylan617/beaufort_jet/runs/roms_avg_ice_500m_w_dvd_constant_forcing_u3c4_kpp_*.nc')
+
+
+# Load in the output from above with xroms
+ds, grid = open_mfroms(path_const_u3c4_kpp)
+add_derivatives(ds, grid)
+
+
+# ---------------------------------------------------------------------------
+# -------------------------- Mixing!  -------------------------------
+# ---------------------------------------------------------------------------
+
+# Calculate distance in km
+# to use for plotting 
+# Can use x_rho, y_rho, x_u, y_u, etc. but those are a 
+# little wonky...sooooo calculate it yourself I guess
+# (I think if we read in the grid directly then they will
+# be fixed? but are weird in the output? No clue but do manually
+# for now)
+dx = ds.x_rho[0,2].values - ds.x_rho[0,1].values
+print('dx: ', dx)
+dy = ds.y_rho[1,0].values - ds.y_rho[0,0].values
+print('dy: ', dy)
+
+len_xi_rho = len(ds.xi_rho.values)
+print('len_xi_rho: ' , len_xi_rho)
+len_eta_rho = len(ds.eta_rho)
+print('len_eta_rho: ', len_eta_rho)
+
+x_rho = np.arange(0, len_xi_rho*dx, dx)
+print('x_rho: ', x_rho[-10:-1])
+print('ds.x_rho: ', ds.x_rho[0,-10:-1].values)
+
+y_rho = np.arange(0, len_eta_rho*dy, dy)
+print('y_rho: ', y_rho[-10:-1])
+print('ds.y_rho: ', ds.y_rho[-10:-1,0].values)
+
+x_u = x_rho[:-1]
+y_u = np.copy(y_rho)
+x_v = np.copy(x_rho)
+y_v = y_rho[:-1]
+
+
+# Plot the domain to figure out where we want to take the 
+# average in the horizontal 
+
+# Set the time index
+time_idx1 = 450
+
+# Make bathymetry levels
+lev_bathy = np.arange(10,110,10)
+lev_u = np.arange(-0.2, 0.2, 0.01)
+
+# Make the figure 
+fig, ax = plt.subplots(1, 2, figsize=(5,5), dpi=200)
+
+# Plot bathymetry
+cs1 = ax[0].contourf(x_rho/1000, y_rho/1000, ds.h, cmap=cmo.deep,
+                     extend='max')
+ax[0].set_title('Bathymetry (m))')
+# Make a colorbar 
+cbar_ax1 = fig.add_axes([0.94,0.55,0.015,0.35])
+fig.colorbar(cs1,ax=ax,extend='both',
+             label='Bathymetry (m)',
+             pad=0.03, cax=cbar_ax1)
+
+# Plot surface u
+cs2 = ax[1].contourf(x_u/1000, y_u/1000, ds.u[time_idx1,-1,:,:], 
+                     lev_u, cmap=cmo.balance, extend='both')
+ax[1].set_title('U (m/s)')
+# Make a colorbar 
+cbar_ax2 = fig.add_axes([0.94,0.08,0.015,0.35])
+fig.colorbar(cs2,ax=ax,extend='both',
+             label='U (m/s)',
+             pad=0.03, cax=cbar_ax2)
+
+# Add other details to each subplot
+for r in range(2):
+    # Set aspect ratio
+    ax[r].set_aspect(1.0)
+
+# Add the time
+fig.suptitle('Time: ' + str(ds.ocean_time[time_idx1].values)[0:19])
+
+# Pick a region to analyze 
+# Box 1
+box_min_x_01 = 0
+box_max_x_01 = 200
+box_min_y_01 = 70
+box_max_y_01 = 210
+# Plot Box 1
+ax[0].plot([box_min_x_01, box_max_x_01, box_max_x_01, box_min_x_01, box_min_x_01],
+        [box_min_y_01, box_min_y_01, box_max_y_01, box_max_y_01, box_min_y_01],
+        color='yellow', linestyle='-', linewidth=2)
+ax[1].plot([box_min_x_01, box_max_x_01, box_max_x_01, box_min_x_01, box_min_x_01],
+        [box_min_y_01, box_min_y_01, box_max_y_01, box_max_y_01, box_min_y_01],
+        color='yellow', linestyle='-', linewidth=2)
+
+fig.canvas.draw()
+ 
+
+# Find where these regions are in terms of xi and eta slicing 
+# Becuase of periodic boundaries, we want the second to last 
+# xi idx and to start at 1 and not 0
+box_min_x_01_idx = ((box_min_x_01 * 1000)+dx)/dx
+print('box_min_x_01_idx: ', box_min_x_01_idx)
+box_max_x_01_idx = ((box_max_x_01 * 1000)-dx)/dx
+print('box_max_x_01_idx: ', box_max_x_01_idx)
+box_min_y_01_idx = (box_min_y_01 * 1000)/dy
+print('box_min_y_01_idx: ', box_min_y_01_idx)
+box_max_y_01_idx = (box_max_y_01 * 1000)/dy
+print('box_max_y_01_idx: ', box_max_y_01_idx)
+
+xi_slice = slice(int(box_min_x_01_idx), int(box_max_x_01_idx))
+eta_slice = slice(int(box_min_y_01_idx), int(box_max_y_01_idx))
+
+# Set the vertical max
+# Note z_rho is negative in ROMS output so it will have 
+# a negative sign in front of it when slicing
+z_slice = 200 # meters
+
+# ------ Calculate Physical & Numerical Mixing ----------
+
+# Get physical mixing from the output
+# Salinity (2-hourly)
+Akr_rho = grid.interp(ds.AKr, 'Z')
+mphys = (Akr_rho*ds.dV).isel(eta_rho = eta_slice, xi_rho = xi_slice).where(ds.z_rho>-z_slice).sum(['eta_rho', 'xi_rho', 's_rho']).compute()
+mphys.attrs = ''
+
+# Temperature (2-hourly)
+Akrt_rho = grid.interp(ds.AKrt, 'Z')
+mphyt = (Akrt_rho*ds.dV).isel(eta_rho = eta_slice, xi_rho = xi_slice).where(ds.z_rho>-z_slice).sum(['eta_rho', 'xi_rho', 's_rho']).compute()
+mphyt.attrs = ''
+
+# Get numerical mixing from the output
+# Numerical mixing (2-hourly)
+mnum_salt_dv = (ds.dye_03*ds.dV).isel(eta_rho = eta_slice, xi_rho=xi_slice).where(ds.z_rho>-z_slice).sum(['eta_rho', 'xi_rho', 's_rho']).compute()
+mnum_temp_dv = (ds.dye_06*ds.dV).isel(eta_rho = eta_slice, xi_rho=xi_slice).where(ds.z_rho>-z_slice).sum(['eta_rho', 'xi_rho', 's_rho']).compute()
+
+
+# ---------------------------------------------------------------------------
+# -------------------------- Save to a NetCDF  -------------------------------
+# ---------------------------------------------------------------------------
+
+# Variables to save: mphys, mphyt, mnum_salt_dv, mnum_temp_dv
+
+roms_phy_num_mix_salt_temp = xr.Dataset(
+    data_vars=dict(
+        mphys=(['ocean_time'], mphys.values),
+        mphyt=(['ocean_time'], mphyt.values),
+        mnum_salt_dv=(['ocean_time'], mnum_salt_dv.values),
+        mnum_temp_dv=(['ocean_time'], mnum_temp_dv.values)
+    ),
+    coords=dict(
+        ocean_time=('ocean_time', ds.ocean_time.values)
+    ),
+    attrs=dict(description='Time-series ROMS output including physical and numerical mixing of salinity and temperature from 2-hourly model output; uses /global/cfs/cdirs/m4572/dylan617/beaufort_jet/runs/roms_avg_ice_500m_w_dvd_constant_forcing_u3c4_kpp_*.nc')
+)
+
+# Add attributes to the 'temperature' data variable
+roms_phy_num_mix_salt_temp['mphys'].attrs['long_name'] = 'Physical mixing of salinity at 2-hourly intervals'
+roms_phy_num_mix_salt_temp['mphys'].attrs['units'] = '(g/kg)\u00b2(m\u00b3/s))'
+roms_phy_num_mix_salt_temp['mphyt'].attrs['long_name'] = 'Physical mixing of temperature at 2-hourly intervals'
+roms_phy_num_mix_salt_temp['mphyt'].attrs['units'] = '(C)\u00b2(m\u00b3/s))'
+
+roms_phy_num_mix_salt_temp['mnum_salt_dv'].attrs['long_name'] = 'Numerical mixing of salinity at 2-hourly interval'
+roms_phy_num_mix_salt_temp['mnum_salt_dv'].attrs['units'] = '(g/kg)\u00b2(m\u00b3/s))'
+roms_phy_num_mix_salt_temp['mnum_temp_dv'].attrs['long_name'] = 'Numerical mixing of temperature at 2-hourly interval'
+roms_phy_num_mix_salt_temp['mnum_temp_dv'].attrs['units'] = '(C)\u00b2(m\u00b3/s))'
+
+# Save to a netcdf
+# Run with ice (change name based on the run being analyzed)
+roms_phy_num_mix_salt_temp.to_netcdf('/global/cfs/cdirs/m4572/bundzis/roms_idealized_beaufort_jet/Post_process_data/roms_phy_num_mix_salt_temp_2hr_ice_500m_const_forc_u3c4_kpp_001.nc')
+# Run without ice
+#roms_phy_num_mix_salt_temp.to_netcdf('/global/cfs/cdirs/m4572/bundzis/roms_idealized_beaufort_jet/Post_process_data/roms_phy_num_mix_salt_temp_2hr_no_ice_500m_const_forc_u3c4_keps_001.nc')
+
+
+
+
+
+
+
